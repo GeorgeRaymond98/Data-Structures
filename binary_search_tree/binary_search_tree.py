929690a710763d9fabf7ad4d59215334e16669fe
George Raymond <georgeraymond98@gmail.com>
2020-04-23 17:37:27 -0400

    fixed insert adn contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,21 +12,53 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # pass
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        elif self.value > target:
+            if self.left != None:
+                return self.left.contains(target)
+            else:
+                return False
+        else:
+            if self.right != None:
+                return self.right.contains(target)
+            else:
+                return False
+
+        # pass
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right != None:
+            return self.right.get_max()
+        else:
+            return self.value
+        # pass
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.left != None:
+            self.left.for_each(cb)
+        if self.right != None:
+            self.right.for_each(cb)
+        # pass
 
     # DAY 2 Project -----------------------
 
